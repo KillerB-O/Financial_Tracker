4e1f110b54f9dbde24bdab97d928352c830c7209
KillerB-O <gautam.joshii1905@gmail.com>
2025-12-03 18:24:40 +0530

    all_set_for prod

--- finpal_repo/backend/app/core/config.py
+++ finpal_repo/backend/app/core/config.py
@@ -1,17 +1,31 @@
 from pydantic_settings import BaseSettings
 from typing import Optional
+from functools  import lru_cache
 
 
 class Settings(BaseSettings):
+
     project_name:str="Finpal_Backend"
+    app_name: str = "FinPal"          
+    environment: str = "development"  
+
     SQLITE_DB_FILE: str="finpal_auth.db"
-    secret_key:str="09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7" 
-    algorithm:str="HS256"
+    DATABASE_URL: Optional[str] = None
+
+    secret_key:str
+    algorithm:str ="HS256"
     access_token_expire_minutes: int= 60*24*7
+
+    min_savings_threshold: float        
+    min_confidence: float              
+    excess_spending_threshold: float      
+
+    
     PENNYWISE_API_URL: Optional[str] = None
     PENNYWISE_API_KEY: Optional[str] = None
     API_URL: str = "http://localhost:8000"
-    DEBUG: bool = True 
+    DEBUG: bool = True
+
     class Config:
             env_file=".env"
             env_file_encoding="utf-8"
@@ -22,4 +36,7 @@ def Settings_sqlitefile() -> str:
 
 settings=Settings()
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{settings.SQLITE_DB_FILE}"
+if settings.DATABASE_URL:
+    SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+else:
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{settings.SQLITE_DB_FILE}"
